cbfc8c6b103df07cdf7ad39593dbea27338ea286
SimpleNick <stepanoff.stepanov-ivan@yandex.ru>
2023-11-15 13:53:06 +0300

    Добавить OwnCloud

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(9,0,11,2);
+$OC_VersionString = '9.0.11';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = [8,2];
+$OC_Build = '2017-11-22T11:06:37+00:00 8f8e9a2a84d40bc11336178b6b7fcf0d41c90dd9';
+$vendor = 'owncloud';
